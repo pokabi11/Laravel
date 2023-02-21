ea7958dfb90b331a8ad8e21c9a00c6f1bc0bd6f2
pokabi11 <hlong2k1@gmail.com>
2023-02-21 11:57:54 +0700

    7 days order

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -25,6 +25,17 @@ class WebController extends Controller
         }
         $category_names = json_encode($category_names);
         $category_products_counts = json_encode($category_products_counts);
+
+        $today = today();
+        $last7 = today()->subDay(6);
+        $last6 = today()->subDay(5);
+        $last5 = today()->subDay(4);
+        $last4 = today()->subDay(3);
+        $last3 = today()->subDay(2);
+        $last2 = today()->subDay(1);
+
+        $today_orders = Order::whereDate("created_at",$today)->count();
+
         return view("welcome",compact('orders_count','orders_sum_grand_total',
         'products_count','total_qty','category_names','category_products_counts'));
     }
